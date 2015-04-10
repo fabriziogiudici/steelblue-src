e4881ad75dbf63e577633bf8c13946fe26201dc0
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2015-04-11 01:47:07 +0200

    Merged.

---
+++ modules/JavaFXBindings/src/main/java/it/tidalwave/role/ui/javafx/impl/PropertyAdapter.java
@@ -0,0 +1,188 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * SteelBlue
+ * http://steelblue.tidalwave.it - hg clone https://bitbucket.org/tidalwave/steelblue-src
+ * %%
+ * Copyright (C) 2015 - 2015 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ *
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.role.ui.javafx.impl;
+
+import javax.annotation.Nonnull;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Executor;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.application.Platform;
+import it.tidalwave.role.ui.BoundProperty;
+import lombok.extern.slf4j.Slf4j;
+
+/***********************************************************************************************************************
+ *
+ * Adapts a {@link BoundProperty} to a JavaFX {@link Property}. It also takes care of threaring issues, making sure that
+ * the JavaFX {@code Property} is updated in the JavaFX UI thread. Conversely, updates on the JavaFX 
+ * {@code BoundProperty} are executed in a separated thread provided by an {@link Executor}.
+ * 
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+@Slf4j
+public class PropertyAdapter<T> implements Property<T>
+  {
+    @Nonnull
+    private final Executor executor;
+
+    @Nonnull
+    private final BoundProperty<T> delegate;
+
+    // FIXME: WEAK LISTENER!!
+    private final List<ChangeListener<? super T>> changeListeners = new ArrayList<>();
+    
+    // FIXME: WEAK LISTENER!!
+    private final List<InvalidationListener> invalidationListeners = new ArrayList<>();
+
+    private T boundValue;
+
+    private final PropertyChangeListener propertyChangeListener = (event) -> 
+      {
+        log.trace("propertyChange({}) - bound value: {}", event, boundValue);
+        
+        if (!Objects.equals(boundValue, event.getNewValue()))
+          {
+            boundValue = (T)event.getNewValue();
+            Platform.runLater(() ->
+              {
+                new ArrayList<>(invalidationListeners)
+                        .forEach(listener -> listener.invalidated(PropertyAdapter.this));
+                new ArrayList<>(changeListeners)
+                        .forEach(listener -> listener.changed(PropertyAdapter.this,
+                                (T)event.getOldValue(), (T)event.getNewValue()));
+              });
+          }
+    };
+
+    public PropertyAdapter (final @Nonnull Executor executor, final @Nonnull BoundProperty<T> delegate)
+      {
+        this.executor = executor;
+        this.delegate = delegate;
+        this.boundValue = delegate.get();
+        delegate.addPropertyChangeListener(propertyChangeListener);
+      }
+
+    @Override
+    public T getValue()
+      {
+        return delegate.get();
+      }
+
+    @Override
+    public void setValue (final T value)
+      {
+        log.debug("setValue({})", value);
+        boundValue = value;
+
+        if (!Objects.equals(value, delegate.get()))
+          {
+            executor.execute(() -> delegate.set(value));
+          }
+      }
+
+    @Override
+    public void addListener (final @Nonnull ChangeListener<? super T> listener)
+      {
+        changeListeners.add(listener);
+      }
+
+    @Override
+    public void removeListener (final @Nonnull ChangeListener<? super T> listener)
+      {
+        changeListeners.remove(listener);
+      }
+
+    @Override
+    public void addListener (final @Nonnull InvalidationListener listener)
+      {
+        invalidationListeners.add(listener);
+      }
+
+    @Override
+    public void removeListener (final @Nonnull InvalidationListener listener)
+      {
+        invalidationListeners.remove(listener);
+      }
+
+    @Override
+    public void bind(ObservableValue<? extends T> observable)
+      {
+        log.warn("bind({})", observable);
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    @Override
+    public void unbind()
+      {
+        log.warn("unbind()");
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    @Override
+    public boolean isBound()
+      {
+        log.warn("isBound()");
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    @Override
+    public void bindBidirectional(Property<T> other)
+      {
+        log.warn("bindBidirectional({})", other);
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    @Override
+    public void unbindBidirectional(Property<T> other)
+      {
+        log.warn("unbindBidirectional({})", other);
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    @Override
+    public Object getBean()
+      {
+        log.warn("getBean()");
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    @Override
+    public String getName()
+      {
+        log.warn("getName()");
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+  }
---
+++ modules/JavaFXBindings/src/main/java/it/tidalwave/ui/javafx/JavaFXApplicationWithSplash.java
@@ -0,0 +1,218 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * SteelBlue
+ * http://steelblue.tidalwave.it - hg clone https://bitbucket.org/tidalwave/steelblue-src
+ * %%
+ * Copyright (C) 2015 - 2015 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ *
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.ui.javafx;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Executor;
+import java.io.IOException;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.application.Application;
+import javafx.application.Platform;
+import com.aquafx_project.AquaFx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lombok.Getter;
+import lombok.Setter;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public abstract class JavaFXApplicationWithSplash extends Application
+  {
+    // Don't use Slf4j and its static logger - give Main a chance to initialize things
+    private final Logger log = LoggerFactory.getLogger(JavaFXApplicationWithSplash.class);
+    
+    private final Splash splash = new Splash(this);
+    
+    @Getter @Setter
+    private boolean maximized;
+    
+    @Getter @Setter
+    private boolean fullScreen;
+    
+    @Getter @Setter
+    private boolean fullScreenLocked;
+
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override
+    public void init()
+      {
+        log.info("init()");
+        splash.init();
+      }
+
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override
+    public void start (final @Nonnull Stage stage)
+      throws Exception
+      {
+        log.info("start({})", stage);
+        final Stage splashStage = new Stage(StageStyle.TRANSPARENT);
+        stage.setMaximized(maximized);
+//        splashStage.setMaximized(maximized); FIXME: doesn't work
+        configureFullScreen(stage);
+//        configureFullScreen(splashStage); FIXME: deadlocks JDK 1.8.0_40
+        
+        if (!maximized && !fullScreen)
+          {
+            splashStage.centerOnScreen();
+          }
+
+        splash.show(splashStage);
+        
+        getExecutor().execute(() -> // FIXME: use JavaFX Worker?
+          {
+            initializeInBackground();
+            Platform.runLater(() ->
+              {
+                try
+                  {
+                    final Parent application = createParent();
+                    final Scene scene = new Scene(application);
+
+                    if (isOSX())
+                      {
+                        setMacOSXLookAndFeel(scene);
+                      }
+
+                    stage.setOnCloseRequest(event -> onClosing());
+                    stage.setScene(scene);
+                    onStageCreated(stage);
+                    stage.show();
+                    splashStage.toFront();
+                    splash.dismiss();
+                  }
+                catch (IOException e)
+                  {
+                    log.error("", e);
+                  }
+              });
+          });
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     *
+     ******************************************************************************************************************/
+    protected void onStageCreated (@Nonnull Stage stage)
+      {
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    protected abstract Parent createParent()
+      throws IOException;
+
+    /*******************************************************************************************************************
+     *
+     *
+     *
+     ******************************************************************************************************************/
+    protected abstract void initializeInBackground();
+
+    /*******************************************************************************************************************
+     *
+     * Invoked when the main {@link Stage} is being closed.
+     *
+     ******************************************************************************************************************/
+    protected void onClosing()
+      {
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    protected Executor getExecutor()
+      {
+        return Executors.newSingleThreadExecutor();
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     *
+     ******************************************************************************************************************/
+    private void setMacOSXLookAndFeel (final @Nonnull Scene scene)
+      {
+        if (isOSX())
+          {
+            log.info("Setting Aqua style");
+            AquaFx.style();
+          }
+      }
+
+    /*******************************************************************************************************************
+     *
+     * TODO: delegate to a provider
+     *
+     ******************************************************************************************************************/
+    public static boolean isOSX()
+      {
+        return System.getProperty("os.name").contains("OS X");
+      }
+    
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    private void configureFullScreen (final @Nonnull Stage stage)
+      {
+        stage.setFullScreen(fullScreen);
+        
+        if (fullScreen && fullScreenLocked)
+          {
+            stage.setFullScreenExitHint("");
+            stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+          }
+      }
+  }
