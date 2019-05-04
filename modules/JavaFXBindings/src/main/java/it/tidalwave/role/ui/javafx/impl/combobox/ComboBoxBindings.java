b921a924428f35f25f1fc9202187b10c5029a085
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2019-05-04 17:55:56 +0200

    Updated copyright notice.

--- modules/JavaFXBindings/src/main/java/it/tidalwave/role/ui/javafx/impl/combobox/ComboBoxBindings.java
+++ modules/JavaFXBindings/src/main/java/it/tidalwave/role/ui/javafx/impl/combobox/ComboBoxBindings.java
@@ -5,7 +5,7 @@
  * SteelBlue
  * http://steelblue.tidalwave.it - git clone git@bitbucket.org:tidalwave/steelblue-src.git
  * %%
- * Copyright (C) 2015 - 2016 Tidalwave s.a.s. (http://tidalwave.it)
+ * Copyright (C) 2015 - 2019 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
  *
  * *********************************************************************************************************************
