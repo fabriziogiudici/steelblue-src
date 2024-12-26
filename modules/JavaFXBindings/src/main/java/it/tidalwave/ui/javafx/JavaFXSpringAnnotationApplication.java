7f491b4d16deb7d22193fe50752fb83ebb957981
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2024-12-26 12:47:37 +0100

    STB-82, STB-83: Role runtime classes no more on the Spring Context; beans are no more installed twice.

--- modules/JavaFXBindings/src/main/java/it/tidalwave/ui/javafx/JavaFXSpringAnnotationApplication.java
+++ modules/JavaFXBindings/src/main/java/it/tidalwave/ui/javafx/JavaFXSpringAnnotationApplication.java
@@ -29,13 +29,10 @@ import javax.annotation.Nonnull;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import it.tidalwave.util.PreferencesHandler;
-import it.tidalwave.role.impl.DefaultContextManager;
-import it.tidalwave.role.spring.spi.AnnotationSpringSystemRoleFactory;
 import it.tidalwave.role.ui.ToolBarModel;
 import it.tidalwave.role.ui.javafx.StackPaneSelector;
 
@@ -47,9 +44,35 @@ import it.tidalwave.role.ui.javafx.StackPaneSelector;
  * @since   1.1-ALPHA-4
  *
  **************************************************************************************************************************************************************/
-@Configuration
 public class JavaFXSpringAnnotationApplication extends AbstractJavaFXSpringApplication
   {
+    static class Beans
+      {
+        @Bean
+        public ThreadPoolTaskExecutor javafxBinderExecutor()
+          {
+            return JavaFXSafeProxyCreator.getExecutor();
+          }
+
+        @Bean
+        public StackPaneSelector stackPaneSelector()
+          {
+            return new StackPaneSelector();
+          }
+
+        @Bean
+        public PreferencesHandler preferencesHandler()
+          {
+            return PreferencesHandler.getInstance();
+          }
+
+        @Bean
+        public ToolBarModel toolBarModel()
+          {
+            return JavaFXSafeProxyCreator.getToolBarModel();
+          }
+      }
+
     // Don't use Slf4j and its static logger - give Main a chance to initialize things
     private final Logger log = LoggerFactory.getLogger(JavaFXSpringApplication.class);
 
@@ -58,42 +81,6 @@ public class JavaFXSpringAnnotationApplication extends AbstractJavaFXSpringAppli
       {
         final var mainClass = getClass();
         log.info("Scanning beans from {}", mainClass);
-        return new AnnotationConfigApplicationContext(mainClass);
-      }
-
-    @Bean
-    public ThreadPoolTaskExecutor javafxBinderExecutor()
-      {
-        return JavaFXSafeProxyCreator.getExecutor();
-      }
-
-    @Bean
-    public StackPaneSelector stackPaneSelector()
-      {
-        return new StackPaneSelector();
-      }
-
-    @Bean
-    public PreferencesHandler preferencesHandler()
-      {
-        return PreferencesHandler.getInstance();
-      }
-
-    @Bean
-    public AnnotationSpringSystemRoleFactory annotationSpringSystemRoleFactory()
-      {
-        return new AnnotationSpringSystemRoleFactory();
-      }
-
-    @Bean
-    public DefaultContextManager defaultContextManager()
-      {
-        return new DefaultContextManager();
-      }
-
-    @Bean
-    public ToolBarModel toolBarModel()
-      {
-        return JavaFXSafeProxyCreator.getToolBarModel();
+        return new AnnotationConfigApplicationContext(mainClass, Beans.class);
       }
   }
